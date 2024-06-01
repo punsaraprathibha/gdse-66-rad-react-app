da3c6d16f76a22563df6eeb1dc2e1fb0c9179eec
punsaraprathibha <punsaraprathibhagpp@gmail.com>
2024-06-01 09:32:00 +0530

    Advances React routing

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,22 @@ import {NavBar} from
         "./view/common/Navbar/NavBar";
 import {MainContent} from "./view/common/MainContent/MainContent";
 import {Footer} from "./view/common/Footer/Footer";
-import {BrowserRouter} from "react-router-dom";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {DefaultLayout} from "./view/common/DefaultContent/DefaultLayout";
+import {Login} from "./view/pages/Login/Login";
 
 function App() {
   return (
     <div>
       <BrowserRouter>
-          <NavBar/>
-          <MainContent/>
-          <Footer/>
+          <Routes>
+              <Route path="/"
+                     Component={DefaultLayout}>
+              </Route>
+              <Route path="/login"
+                    Component={Login}>
+              </Route>
+          </Routes>
       </BrowserRouter>
     </div>
   );
---
+++ src/view/pages/Login/Login.tsx
@@ -0,0 +1,9 @@
+import {Component} from "react";
+
+export class Login extends Component {
+    render() {
+        return (
+            <h1>This is Login Page</h1>
+        );
+    }
+}
