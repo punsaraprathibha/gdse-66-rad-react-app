8ebbd06841c535559db314843d9b300ec466b0d1
punsaraprathibha <punsaraprathibhagpp@gmail.com>
2024-06-01 11:29:37 +0530

    Home Page Creation

--- src/view/pages/Home/Home.tsx
+++ src/view/pages/Home/Home.tsx
@@ -1,4 +1,6 @@
 import {Component} from "react";
+import spinash
+    from '../../../images/products/spinach.png'
 
 export class Home extends Component {
     render() {
@@ -20,7 +22,41 @@ export class Home extends Component {
                                    flex
                                    justify-center
                                    items-center">
-                       01
+                       <div>
+                           <img className="h-[88px]
+                                           w-[88px]"
+                                src={spinash} alt=""/>
+                       </div>
+                       <div className="flex">
+                           <div>
+                               <h3 className="text-secondary
+                                              text-[12px]
+                                              pl-2"
+                               >Spinash</h3>
+                           </div>
+                           <div className="bg-yellow-300
+                                           ml-1
+                                           p-[0.3px]
+                                           rounded-lg
+                                           pr-2">
+                               <h3 className="text-[12px]
+                                               pl-2">
+                                   200 <small className="text-[7px]">
+                                   LKR</small>
+                               </h3>
+                           </div>
+                           <div className="flex justify-center">
+                               <button className="w-full
+                                                  mt-1
+                                                  p-[2.4px]
+                                                  bg-secondary
+                                                  text-[8px]
+                                                  border-gray-500
+                                                  border-[0.5px]">
+                                   Add to Cart
+                               </button>
+                           </div>
+                       </div>
                    </div>
                    <div className="w-28 h-32
                                    bg-blue-300
