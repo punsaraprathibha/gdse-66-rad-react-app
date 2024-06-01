9c5d3b5881d090dbf07ef214cbab857182ebdc09
punsaraprathibha <punsaraprathibhagpp@gmail.com>
2024-06-01 10:55:33 +0530

    Home Page Creation

--- src/view/pages/Home/Home.tsx
+++ src/view/pages/Home/Home.tsx
@@ -3,9 +3,54 @@ import {Component} from "react";
 export class Home extends Component {
     render() {
         return (
-            <>
-            <h1>This is Home Component</h1>
-            </>
+           <div className="flex">
+               {/*Row 01  */}
+               <div className="flex
+                               flex-wrap
+                               ml-[1px]
+                               mt-10
+                               mb-5
+                               justify-center
+                               items-center
+                               mx-auto">
+                   <div className="w-28 h-32
+                                   bg-blue-300
+                                   mr-2
+                                   mb-2
+                                   flex
+                                   justify-center
+                                   items-center">
+                       01
+                   </div>
+                   <div className="w-28 h-32
+                                   bg-blue-300
+                                   mr-2
+                                   mb-2
+                                   flex
+                                   justify-center
+                                   items-center">
+                       02
+                   </div>
+                   <div className="w-28 h-32
+                                   bg-blue-300
+                                   mr-2
+                                   mb-2
+                                   flex
+                                   justify-center
+                                   items-center">
+                       03
+                   </div>
+                   <div className="w-28 h-32
+                                   bg-blue-300
+                                   mr-2
+                                   mb-2
+                                   flex
+                                   justify-center
+                                   items-center">
+                       04
+                   </div>
+               </div>
+           </div>
         );
     }
 }
