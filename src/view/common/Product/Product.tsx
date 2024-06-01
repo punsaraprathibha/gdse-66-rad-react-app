fca9d034c88ac8f1228659e0cfaae1441597bb36
punsaraprathibha <punsaraprathibhagpp@gmail.com>
2024-06-01 14:51:35 +0530

    Added more changes

--- src/view/common/Product/Product.tsx
+++ src/view/common/Product/Product.tsx
@@ -2,13 +2,12 @@ import {Component} from "react";
 import spinash from "../../../images/products/spinach.png";
 
 interface ProductProps {
-    data: null;
+    data: any;
 }
 export class Product extends Component<ProductProps> {
     render() {
 
         const {data} = this.props;
-        // @ts-ignore
         const image = require('../../../images/products/' + data.image)
         return (
             <div className="w-28 h-32 mr-2 mb-2 justify-center items-center border-gray-500 border-[0.5px]">
@@ -17,10 +16,10 @@ export class Product extends Component<ProductProps> {
                 </div>
                 <div className="flex">
                     <div>
-                        <h3 className="text-secondary text-[12px] pl-2">Spinach</h3>
+                        <h3 className="text-secondary text-[12px] pl-2">{data.name}</h3>
                     </div>
                     <div className="bg-yellow-300 ml-1 p-[0.3px] rounded-lg pr-2">
-                        <h3 className="text-[12px] pl-2">200 <small className="text-[7px]">LKR</small></h3>
+                        <h3 className="text-[12px] pl-2">{data.price} <small className="text-[7px]">{data.currency}</small></h3>
                     </div>
                 </div>
                 <div className="flex justify-center">
