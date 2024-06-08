41fdc7b87f1592e9d8763b7cca25bb2f35e7e247
punsaraprathibha <punsaraprathibhagpp@gmail.com>
2024-06-08 15:38:23 +0530

    Added Shopping cart modifications

--- src/view/pages/ShoppingCart/ShoppingCart.tsx
+++ src/view/pages/ShoppingCart/ShoppingCart.tsx
@@ -19,7 +19,44 @@ export class ShoppingCart extends Component<ShoppingCartProps> {
                                 border-black
                                 border-[0.5px]
                                 px-1">Id</th>
+                        <th className="text-[9px]
+                                font-normal
+                                border-black
+                                border-[0.5px]
+                                px-1">Name</th>
+                        <th className="text-[9px]
+                                font-normal
+                                border-black
+                                border-[0.5px]
+                                px-1">Unit Price</th>
+                        <th className="text-[9px]
+                                font-normal
+                                border-black
+                                border-[0.5px]
+                                px-1">Quantity</th>
+                        <th className="text-[9px]
+                                font-normal
+                                border-black
+                                border-[0.5px]
+                                px-1">Total Price</th>
                     </tr>
+                    {
+                        this.props.itemsList
+                            .length === 0 ?
+                            <tr>
+                                <td colSpan={5}
+                                     className="border-black
+                                                border-[0.5px]
+                                                px-1">
+                                    <p className="text-[8px]
+                                                  text-center">
+                                        No Items to Display!
+                                    </p>
+                                </td>
+                            </tr>
+                            :
+                            <tr></tr>
+                    }
                 </table>
             </div>
         );
