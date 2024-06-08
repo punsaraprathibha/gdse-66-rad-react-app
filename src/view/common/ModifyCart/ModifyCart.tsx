b5a157451294971907f9e0d12f861f4fbe4c7c5c
punsaraprathibha <punsaraprathibhagpp@gmail.com>
2024-06-08 14:11:11 +0530

    Added Modify cart update

--- src/view/common/ModifyCart/ModifyCart.tsx
+++ src/view/common/ModifyCart/ModifyCart.tsx
@@ -2,7 +2,7 @@ import {Component} from "react";
 import {CartItem} from "../../../model/CartItem";
 
 interface ModifyCartProps {
-
+    data: any
 }
 interface ModifyCartState {
     itemCount: number
@@ -18,6 +18,27 @@ export class ModifyCart extends Component<ModifyCartProps, ModifyCartState> {
             itemCount: 1
         }
     }
+
+    componentDidMount() {
+        const {itemCount}
+            = this.state;
+
+        if (this.props.data.isAdded) {
+            if (!ModifyCart.itemsList
+                .find(item=>
+                    item.product.id ===
+                    this.props.data.product.id)) {
+                ModifyCart.itemsList.push(
+                    {
+                        product: this.props.data.product,
+                        itemCount: itemCount
+                    }
+                );
+                console.log(ModifyCart.itemsList);
+            }
+        }
+    }
+
     render() {
 
         let {itemCount}
