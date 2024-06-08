25f2e15aba52f43b84ece663b28611b43f23f706
punsaraprathibha <punsaraprathibhagpp@gmail.com>
2024-06-08 15:24:25 +0530

    Added Shopping cart modifications

--- src/view/pages/ShoppingCart/ShoppingCart.tsx
+++ src/view/pages/ShoppingCart/ShoppingCart.tsx
@@ -8,7 +8,20 @@ interface ShoppingCartProps {
 export class ShoppingCart extends Component<ShoppingCartProps> {
     render() {
         return (
-            <h1>This is Shopping Cart!</h1>
+            <div className="flex
+                justify-center
+                items-center
+                min-h-screen">
+                <table className="w-full mx-6">
+                    <tr className="bg-gray-400">
+                        <th className="text-[9px]
+                                font-normal
+                                border-black
+                                border-[0.5px]
+                                px-1">Id</th>
+                    </tr>
+                </table>
+            </div>
         );
     }
 }
