615f47cce894cc6bd665b7067da6bbc0bfd149e5
punsaraprathibha <punsaraprathibhagpp@gmail.com>
2024-06-29 15:29:17 +0530

    functional component

--- client/src/view/pages/Home/Home.tsx
+++ client/src/view/pages/Home/Home.tsx
@@ -1,36 +1,23 @@
-import {Component} from "react";
+import {useEffect, useState} from "react";
 import axios from 'axios';
 import {Product} from "../../common/Product/Product";
-import {Simulate} from "react-dom/test-utils";
-import error = Simulate.error;
 
-export class Home extends Component {
+export const Home = () => {
+    const [data, setData] = useState([]);
 
-    private api: any;
+    const api = axios.create({
+           baseURL: `http://localhost:4000`});
 
-    constructor(props: {}) {
-        super(props);
-        this.api = axios.create({
-           baseURL: `http://localhost:4000`
-        });
-        this.state = {
-            data: []
-        }
-    }
-    componentDidMount() {
-        this.fetchData();
-    }
-    fetchData = async () => {
+    const fetchData = async () => {
         try {
             // let response = await fetch('./product-data.json');
             // let jsonData = await response.json();
-            this.api
-                .get('/products/all')
+            api.get('/products/all')
                 .then((res: { data: any }) => {
                     const jsonData = res.data;
-                    this.setState({data: jsonData});
+                    setData(jsonData);
                 }).catch((error: any) => {
-                    console.error("Axios Error:", error);
+                console.error("Axios Error:", error);
             });
         } catch (error) {
             console.error(
@@ -39,13 +26,14 @@ export class Home extends Component {
         }
     }
 
-    render() {
-        // @ts-ignore
-        const {data} = this.state;
-        return (
-            <div className="flex">
-                {/*Row 01  */}
-                <div className="flex
+    useEffect(()=> {
+       fetchData();
+    });
+
+    return (
+        <div className="flex">
+            {/*Row 01  */}
+            <div className="flex
                                flex-wrap
                                ml-[1px]
                                mt-10
@@ -53,14 +41,13 @@ export class Home extends Component {
                                justify-center
                                items-center
                                mx-auto">
-                    {
-                        data.map((product: any) => (
-                            <Product key={product.id}
-                                     data={product}/>
-                        ))
-                    }
-                </div>
+                {
+                    data.map((product: any) => (
+                        <Product key={product.id}
+                                 data={product}/>
+                    ))
+                }
             </div>
-        );
-    }
-}
+        </div>
+    );
+};
